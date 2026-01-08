8a51837c2156437001215f98937da222bf806e6a
743hw4n <taehwan011@gmail.com>
2026-01-08 17:42:43 +0900

    fix(storages.py): S3Boto3Storage를 S3Storage로 변경하여 호환성 개선

--- backend/config/storages.py
+++ backend/config/storages.py
@@ -1,9 +1,9 @@
-from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3 import S3Storage
 
-class MediaStorage(S3Boto3Storage):
+class MediaStorage(S3Storage):
     location = 'media'
     file_overwrite = False
 
-class StaticStorage(S3Boto3Storage):
+class StaticStorage(S3Storage):
     location = 'static'
     file_overwrite = False
