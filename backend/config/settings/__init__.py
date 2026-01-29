ab670e56fe3e0d8a9c2c001ae352f553f2b8c28c
743hw4n <taehwan011@gmail.com>
2026-01-30 01:16:53 +0900

    refactor: DJANGO_ENV 유효성 검사 추가

--- backend/config/settings/__init__.py
+++ backend/config/settings/__init__.py
@@ -2,6 +2,9 @@ import os
 
 env = os.getenv('DJANGO_ENV', 'local')
 
+if env not in ['production', 'local']:
+    raise RuntimeError(f'Invalid DJANGO_ENV: {env}')
+
 if env == 'production':
     from .production import *
 else:
